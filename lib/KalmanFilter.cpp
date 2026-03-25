1c7fe3103d654691517398298282c969a826afcb
joanahabib <121553901+joanahabib@users.noreply.github.com>
2026-03-25 09:42:45 +0200

    Update KalmanFilter.cpp

--- lib/KalmanFilter.cpp
+++ lib/KalmanFilter.cpp
@@ -24,3 +24,32 @@ void KalmanFilter::initialize(
     P = P0;
 }
 
+void KalmanFilter::predict() {
+    x = multiply(A, x);
+    P = add(multiply(A, P), transpose(A)), Q);
+}
+
+void KalmanFilter::update(const std::vector<double>& z) {
+    auto Ht = transpose(H);
+    auto S = add(multiply(multiply(H, P), Ht), R);
+    auto K = multiply(multiply(P, Ht), inverse(S));
+
+    auto y = z;
+    auto Hx = multiply(H, x);
+    for (size_t i = 0; i < y.size(), i++) {
+        y[i] -= Hx[i];
+    }
+    auto K_y = multiply(K,y);
+    for (size_t i = 0; i < x.size(), i++) {
+        x[i] = K_y[i];
+    }
+    auto I = identity(stateDim);
+    P = multiply(subtract(I, multiply(K, H)), P);
+}
+
+std::vector<double> KalmanFilter::getState() const {
+    return x;
+}
+
+
+
